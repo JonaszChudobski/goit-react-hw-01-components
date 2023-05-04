58db5f1c4c66086e6243693c5f09db45b5c70ef1
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-05-04 12:57:22 +0200

    Updates

---
+++ precache-manifest.11300676e6d89f4646adee3b48eb99db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71bc130f8b5db2b8a6e4",
+    "url": "/goit-react-hw-01-components/static/css/main.1eaae88e.chunk.css"
+  },
+  {
+    "revision": "71bc130f8b5db2b8a6e4",
+    "url": "/goit-react-hw-01-components/static/js/main.71bc130f.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/goit-react-hw-01-components/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "12def28e4aa6b79a7681",
+    "url": "/goit-react-hw-01-components/static/js/2.12def28e.chunk.js"
+  },
+  {
+    "revision": "f0bf75318e0ceb1dd9eb",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.f0bf7531.js"
+  },
+  {
+    "revision": "8b17784ed2ece42c4e6759caa8b774fa",
+    "url": "/goit-react-hw-01-components/index.html"
+  }
+];
